83d967c413ebb01cd1106f44a3c05cbb2cb6b009
BraveNewDeFi <80540658+BraveNewDeFi@users.noreply.github.com>
2023-01-23 23:40:44 -0500

    Updated with config file
    Docs-only setup, so it's just the documentation. No supporting website, no blog, etc.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,22 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://docs.nexusmutual.io',
+  title: 'Nexus Mutual Documentation',
+  tagline: 'Your guide to the crypto insurance alternative',
+  url: 'https://docs.nexusmutual.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/NXM-Spinner-Green.svg',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  organizationName: 'NexusMutual', // Usually your GitHub org/user name.
+  projectName: 'NexusMutual/docs', // Usually your repo name.
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -33,19 +29,9 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -57,22 +43,21 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Nexus Mutual',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'NXM-Spinner-Green',
+          src: 'img/NXM-Spinner-Green.svg',
         },
         items: [
+          //{to: '/blog', label: 'Blog', position: 'left'},
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
+            href: 'https://nexusmutual.io/',
+            label: 'Website',
+            position: 'right',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            href: 'https://app.nexusmutual.io/home',
+            label: 'Nexus Mutual UI',
             position: 'right',
           },
         ],
@@ -81,11 +66,19 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: 'Governance',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Forum',
+                href: 'https://forum.nexusmutual.io/',
+              },
+              {
+                label: 'Protocol Governance',
+                href: 'https://app.nexusmutual.io/governance',
+              },
+              {
+                label: 'DAO Snapshot Governance',
+                href: 'https://snapshot.org/#/community.nexusmutual.eth',
               },
             ],
           },
@@ -93,34 +86,51 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: 'https://discord.com/invite/xxFaAEn',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/NexusMutual',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/nexus-mutual',
               },
             ],
           },
           {
-            title: 'More',
+            title: 'Developers',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/NexusMutual',
+              },
+              {
+                label: 'Bug Bounty',
+                href: 'https://immunefi.com/bounty/nexusmutual/',
+              },
+            ],
+          },
+          {
+            title: 'Publishing',
             items: [
               {
                 label: 'Blog',
-                to: '/blog',
+                to: 'https://nexusmutual.io/blog',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'Nexus Mutual Newsletter',
+                href: 'https://nexusmutual.substack.com/',
+              },
+              {
+                label: 'YouTube',
+                href: 'https://www.youtube.com/@nexus_mutual',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        
       },
       prism: {
         theme: lightCodeTheme,
