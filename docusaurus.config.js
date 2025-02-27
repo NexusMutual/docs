0d04c2ec0dd0740bec93549102b7665f350b27e1
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2025-02-27 18:32:22 +0200

    chore: update docusaurus config to include latex support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,8 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -28,7 +30,11 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: { routeBasePath: '/' },
+        docs: { 
+          routeBasePath: '/',
+          remarkPlugins: [math],
+          rehypePlugins: [katex]
+        },
         blog: false,
         googleTagManager: { containerId: 'GTM-WKL6F7R' },
         theme: {
@@ -43,6 +49,16 @@ const config = {
       require.resolve('@cmfcmf/docusaurus-search-local'),
       { indexDocs: true, indexBlog: false },
     ]
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig:
